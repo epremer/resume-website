d50a67022adff36a2e395cde9d3a5de5223048ed
Lizz <elizabethpremer@gmail.com>
2019-08-16 18:40:54 -0700

    init - from divjoy.com
    Using a new, free js templating tool that utilizes drag and drop capabilities of react components which can be exported and customized

---
+++ src/pages/cakecreations/index.js
@@ -0,0 +1,49 @@
+import React from "react";
+import CtaSection3 from "./../../components/CtaSection3";
+import ContentCardsSection from "./../../components/ContentCardsSection";
+import TestimonialsSection from "./../../components/TestimonialsSection";
+import CtaSection2 from "./../../components/CtaSection2";
+import { useRouter } from "./../../util/router.js";
+import "./styles.scss";
+
+function CakeCreationsPage(props) {
+  const router = useRouter();
+
+  return (
+    <>
+      <CtaSection3
+        color="dark"
+        size="medium"
+        title="For the love of god buy our product"
+        buttonText="Okay fine"
+        buttonOnClick={() => {
+          router.push("/pricing");
+        }}
+      />
+      <ContentCardsSection
+        color="white"
+        size="medium"
+        title="Featured Content"
+        subtitle=""
+      />
+      <TestimonialsSection
+        color="white"
+        size="medium"
+        title="Here's what people are saying"
+        subtitle=""
+      />
+      <CtaSection2
+        color="white"
+        size="medium"
+        title="Ready to get started?"
+        subtitle="Or please get started now even if you don't feel ready. We're just really going to need you to get started. Click it. Click the button."
+        buttonText="Get Started"
+        buttonOnClick={() => {
+          router.push("/pricing");
+        }}
+      />
+    </>
+  );
+}
+
+export default CakeCreationsPage;
